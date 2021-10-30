4ae08f51ce2ab381a12f63c825c016f081a1c1a1
Shin-Taro <sh.takahashi.113@gmail.com>
2021-10-30 21:19:47 +0900

    [add]add to styles Home for SP
    SP用スタイルをHomeコンポーネントに追加

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import media from "styled-media-query";
 
 const Home = () => {
   return(
@@ -14,12 +15,20 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   height: calc(100vh - 140px);
+
+  ${media.lessThan("medium")`
+    height: calc(100vh - 120px);
+  `}
 `;
 
 const Text = styled.p`
   transform: translateY(-140px);
   line-height: 60px;
   font-size: 40px;
+  ${media.lessThan("medium")`
+    transform: translateY(-120px);
+    font-size: 26px;
+  `}
 `;
 
 export default Home;
