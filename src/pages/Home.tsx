1a47531938c6ca1e84d9bc0bb878fcd6a1de9122
Shin-Taro <sh.takahashi.113@gmail.com>
2021-10-30 21:16:12 +0900

    [add]implement HOME page
    Homeページを実装

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 
 const Home = () => {
   return(
-    <div>home</div>
+    <Wrapper>
+      <Text>Wellcome to<br/>　Shintaro's Portfolio</Text>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 140px);
+`;
+
+const Text = styled.p`
+  transform: translateY(-140px);
+  line-height: 60px;
+  font-size: 40px;
+`;
 
 export default Home;
