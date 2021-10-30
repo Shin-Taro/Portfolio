5435d3b6af0b411441c1856e7febcabd7f22f9fd
Shin-Taro <sh.takahashi.113@gmail.com>
2021-10-30 18:00:47 +0900

    [add]implement Header component details
    Headerコンポーネントを実装

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import media from 'styled-media-query';
+import Navbar from './Navbar';
+import NavMenu from './NavMenu';
 
 const Header = () => {
   return(
-    <header>
-      <Link to="/">HOME</Link>
-      <Link to="/about">ABOUT</Link>
-      <Link to="/skill">SKILL</Link>
-      <Link to="/works">WORKS</Link>
-    </header>
+    <Wrapper>
+      <Title>Shintaro's Portfolio</Title>
+      <Navbar/>
+      <NavMenu/>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10;
+  width: 100vw;
+  height: 80px;
+  background-color: white;
+  ${media.lessThan("medium")`
+    height: 70px;
+  `}
+`;
+
+const Title = styled.h1`
+  display: none;
+  margin-left: 20px;
+  line-height: 70px;
+  font-size: 18px;
+  ${media.lessThan("medium")`
+    display: block;
+  `}
+`;
 
 export default Header;
