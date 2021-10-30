3410f62288d0ee09e5a13bf741ae59f61b8f947f
Shin-Taro <sh.takahashi.113@gmail.com>
2021-10-30 15:30:53 +0900

    [add]apply styled-reset
    App.tsx内でグローバルスタイルを宣言し、resetCSSを適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 import Header from './components/Header';
 import Pages from './pages/Pages';
 
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  *, *::after, *::before {
+    box-sizing: border-box;
+  }
+`;
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header/>
-      <Pages/>
-    </BrowserRouter>
+    <>
+      <GlobalStyle/>
+      <BrowserRouter>
+        <Header/>
+        <Pages/>
+      </BrowserRouter>
+    </>
   );
 };
 
