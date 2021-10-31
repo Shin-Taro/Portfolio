6734708fc36b00dc3859484eaf314282a3758eb7
Shin-Taro <sh.takahashi.113@gmail.com>
2021-10-31 17:42:24 +0900

    [add]add unique id to skill list
    Skillコンポーネント内のskills配列に一意のIDプロパティを追加

--- src/pages/Skill.tsx
+++ src/pages/Skill.tsx
@@ -7,10 +7,10 @@ const Skill = () => {
     const list: JSX.Element[] = skills.map((item, index) => {
       const levels: any[] = [];
       for (let i = 0; i < item.level; i++) {
-        levels.push(<span key={i}>★</span>);
+        levels.push(<span key={item.id + "level"}>★</span>);
       }
       return(
-        <Item key={index + item.name}>
+        <Item key={item.id}>
           <h1>{item.name}</h1>
           <p>{levels}</p>
           <p>{item.desc}</p>
@@ -31,6 +31,7 @@ const Skill = () => {
 };
 
 type SkillItem = {
+  id: number,
   name: string,
   level: number,
   desc: string
@@ -40,46 +41,55 @@ type SkillList = SkillItem[]
 
 const skills: SkillList = [
   {
+    id: 1,
     name: "HTML",
     level: 5,
     desc: "実務でWebサイト制作をしています。"
   },
   {
+    id: 2,
     name: "CSS",
     level: 5,
     desc: "大抵のスタイリングは問題無くできます。やればやるほど奥が深いです。"
   },
   {
+    id: 3,
     name: "SCSS",
     level: 4,
     desc: "実務でCSSを書くときはsassで書いてます。"
   },
   {
+    id: 4,
     name: "JavaScript",
     level: 4,
     desc: "やっとクセをつかんできた感じがします。"
   },
   {
+    id: 5,
     name: "jQuery",
     level: 4,
     desc: "サイト制作でアニメーションや簡単な動きをつける際に使用しています。"
   },
   {
+    id: 6,
     name: "React",
     level: 3,
     desc: "モダンな技術でアプリ開発がしたいと思い勉強中です。"
   },
   {
+    id: 7,
     name: "jest",
     level: 2,
     desc: "Reactで作ったアプリはjeatでテストを書いています。実務でテストを書いたことがないので勉強中です。"
   },
   {
+    id: 8,
     name: "TypeScript",
     level: 1,
     desc: "最近手を出し始めました。まだまだ使いこなせていません。"
   },
   {
+    id: 9,
     name: "Firebase",
     level: 2,
     desc: "個人開発の際にHosting・Firestore・Authentication等を利用しています。"
