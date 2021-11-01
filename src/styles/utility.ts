31a7d88293871085aadcda48e8f684e458a81392
Shin-Taro <sh.takahashi.113@gmail.com>
2021-11-02 04:15:33 +0900

    [add]add utility Text styles
    utility.tsにテキスト系のスタイルを追加

--- src/styles/utility.ts
+++ src/styles/utility.ts
@@ -5,8 +5,25 @@ export const Wrapper = styled.div`
   min-height: 100vh;
   padding-top: ${props => props.theme.layout.height.head.large};
   padding-bottom: ${props => props.theme.layout.height.foot.large};
-  background-color: ${props => props.theme.color[props.color]};
+  background-color: ${props => props.theme.color.dark};
   ${media.lessThan("medium")`
     padding-top: ${props => props.theme.layout.height.head.small};
+  `}
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+  color: ${props => props.theme.color.gray};
+  font-size: ${props => props.theme.font.size.title.large};
+  ${media.lessThan("medium")`
+    font-size: ${props => props.theme.font.size.title.small};
+  `}
+`;
+
+export const Text = styled.p`
+  color: ${props => props.theme.color.gray};
+  font-size: ${props => props.theme.font.size.secondary.large};
+  ${media.lessThan("medium")`
+    font-size: ${props => props.theme.font.size.secondary.small};
   `}
 `;
