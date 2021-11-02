966d1528d6310af760bec5dfe89f8540ecbe3506
Shin-Taro <sh.takahashi.113@gmail.com>
2021-11-02 16:01:13 +0900

    [update]set default color
    utilityで指定している色のスタイルにデフォルト値を設定し、呼び出し側からpropsで指定がない場合はデフォルト値を使用する

--- src/styles/utility.ts
+++ src/styles/utility.ts
@@ -5,7 +5,7 @@ export const Wrapper = styled.div`
   min-height: 100vh;
   padding-top: ${props => props.theme.layout.height.head.large};
   padding-bottom: ${props => props.theme.layout.height.foot.large};
-  background-color: ${props => props.theme.color.dark};
+  background-color: ${props => props.theme.color[props.color] || props.theme.color.dark};
   ${media.lessThan("medium")`
     padding-top: ${props => props.theme.layout.height.head.small};
   `}
@@ -16,13 +16,13 @@ export const Container = styled.div`
   margin: 0 auto ${props => props.theme.layout.margin.top.large};
   ${media.lessThan("medium")`
     width: ${props => props.theme.layout.width.large};
-    margin: 0 auto ${props => props.theme.layout.margin.top.small}
+    margin: 0 auto ${props => props.theme.layout.margin.top.small};
   `}
 `;
 
 export const Title = styled.h1`
   text-align: center;
-  color: ${props => props.theme.color[props.color]};
+  color: ${props => props.theme.color[props.color] || props.theme.color.white};
   font-size: ${props => props.theme.font.size.title.large};
   ${media.lessThan("medium")`
     font-size: ${props => props.theme.font.size.title.small};
@@ -31,7 +31,7 @@ export const Title = styled.h1`
 
 export const SubTitle = styled.h1`
   text-align: center;
-  color: ${props => props.theme.color[props.color]};
+  color: ${props => props.theme.color[props.color] || props.theme.color.white};
   font-size: ${props => props.theme.font.size.subTitle.large};
   ${media.lessThan("medium")`
     font-size: ${props => props.theme.font.size.subTitle.small};
@@ -40,7 +40,7 @@ export const SubTitle = styled.h1`
 
 export const Text = styled.p`
   line-height: 2em;
-  color: ${props => props.theme.color[props.color]};
+  color: ${props => props.theme.color[props.color] || props.theme.color.white};
   font-size: ${props => props.theme.font.size.primary.large};
   ${media.lessThan("medium")`
     font-size: ${props => props.theme.font.size.primary.small};
@@ -48,7 +48,7 @@ export const Text = styled.p`
 `;
 
 export const SmallText = styled.p`
-  color: ${props => props.theme.color[props.color]};
+  color: ${props => props.theme.color[props.color] || props.theme.color.white};
   font-size: ${props => props.theme.font.size.secondary.large};
   ${media.lessThan("medium")`
     font-size: ${props => props.theme.font.size.secondary.small};
